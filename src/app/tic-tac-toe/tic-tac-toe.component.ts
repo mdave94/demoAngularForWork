cb90621220410e218e0ab21e9555c84c435c4407
Molnár Dávid <dmolnar@GLOBENET.LOCAL>
2020-03-09 11:58:24 +0100

    update

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -9,7 +9,7 @@ import { stringify } from 'querystring';
 })
 export class TicTacToeComponent implements OnInit {
   table = ["","","","","","","","",""];
-  player = false; // false == X    true == O
+  player = false;  // false == X    true == O
  
   sign;
   
@@ -22,7 +22,7 @@ export class TicTacToeComponent implements OnInit {
   resetGame(): void {
    
     var id : string;
-    document.getElementById("1").innerHTML;
+    
     for (var i =0;i<this.table.length;i++){
    
         id = String(i);
@@ -41,24 +41,55 @@ export class TicTacToeComponent implements OnInit {
     }else{
       this.sign = "X";
     }
-
-      console.log(" SIGN :"+this.sign);
+    
   }
 
   draw($event){
+    
+
     this.currentPlayer();
 
-    
-
-    $event.toElement.innerText = this.table[0];
-    if($event.toElement.innerText == ""){
+    if($event.toElement.innerText === ""){
+     
+      this.table[$event.target.id] = this.sign;
       $event.toElement.innerText = this.sign;
+      
+     
     }
-    
+    this.checkWin();
 
     
   }
 
+   checkWin(){
+    
+    if((this.table[0] === "X" &&
+      this.table[1] === "X" &&
+      this.table[2] === "X" ) || (this.table[0] === "O" &&
+      this.table[1] === "O" &&
+      this.table[2] === "O" )) { alert(this.sign+"Won the game")}
+    else if (
+      (this.table[3] === "X" &&
+      this.table[4]=== "X" &&
+      this.table[5] === "X"
+    ) ||(this.table[3] === "O" &&
+      this.table[4] === "O" &&
+      this.table[5] === "O" ) ){ alert(this.sign+"Won the game")}
+     else if (
+      (this.table[6] === "X" &&
+      this.table[7] === "X" &&
+      this.table[8] === "X"
+    ) || (this.table[6] === "O" &&
+    this.table[7] === "O" &&
+    this.table[8] === "O" )){ alert(this.sign+"Won the game")}
+     
+    console.log("checkwin:");
+    console.log(this.table);
+    
+    
+  }
+  
+  
 
   onClick($event){
     this.draw($event);
